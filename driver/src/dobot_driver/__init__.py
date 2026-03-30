8e0c1454beb01489df2a6373642247bc33fa9eb5
zhao <20024592+agentzhao@users.noreply.github.com>
2026-03-30 14:05:45 +0800

    fix: export

--- driver/src/dobot_driver/__init__.py
+++ driver/src/dobot_driver/__init__.py
@@ -1,17 +1,5 @@
-from .dobot_api import DobotApi, DobotApiDashboard, DobotApiMove, MyType, alarmAlarmJsonFile
-from .dobot_client import CartesianPose, DobotDeviceClient, NUMBER_PATTERN
-from .m1pro import M1Pro, M1ProArm, M1ProTemp
+from .m1pro import M1Pro
 
 __all__ = [
-    "CartesianPose",
-    "DobotApi",
-    "DobotApiDashboard",
-    "DobotApiMove",
-    "DobotDeviceClient",
     "M1Pro",
-    "M1ProArm",
-    "M1ProTemp",
-    "MyType",
-    "NUMBER_PATTERN",
-    "alarmAlarmJsonFile",
 ]
